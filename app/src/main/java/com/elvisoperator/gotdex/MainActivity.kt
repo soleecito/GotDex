6eb839c8523b44c0b2478a40e4ad700da8c6f497
vao-source-code <vorue@alumno.unlam.edu.ar>
2021-09-11 13:42:18 -0300

    Prueba de API
    Muestra error desconocido debe ser resuelto

--- app/src/main/java/com/elvisoperator/gotdex/MainActivity.kt
+++ app/src/main/java/com/elvisoperator/gotdex/MainActivity.kt
@@ -3,10 +3,86 @@ package com.elvisoperator.gotdex
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.elvisoperator.gotdex.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() , SearchView.OnQueryTextListener{
+
+    private lateinit var binding :ActivityMainBinding
+    private lateinit var adapter: CharacterAdapter
+    private val characterImages = mutableListOf<String>()
+    private val characterSlug = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.searchCharacters.setOnQueryTextListener(this)
+        initRecyclerView()
 
+
+    }
+
+    private fun initRecyclerView() {
+        adapter = CharacterAdapter(characterSlug , characterImages)
+        binding.recyclerViewCharacter.layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewCharacter.adapter = adapter
+    }
+
+    private fun getRetrofit (): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://api.got.show/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByName(query: String){
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getCharacterByName("bySlug/$query")
+            val character: Book? = call.body()
+            runOnUiThread {
+                if(call.isSuccessful){
+                    val slug = character?.slug
+                    val image = character?.image
+
+                    if(!slug.isNullOrEmpty() && !image.isNullOrEmpty() ){
+                        characterImages.clear()
+                        characterSlug.clear()
+                        characterImages.add(image)
+                        characterSlug.add(slug)
+                        adapter.notifyDataSetChanged()
+
+                    }
+
+
+                }else{
+                    //show error
+                }
+
+            }
+
+        }
+    }
+
+    private fun showError(){
+        Toast.makeText(this, "Error" ,Toast.LENGTH_LONG).show()
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if (!query.isNullOrEmpty()) {
+            searchByName(query.lowercase())
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+      return true
     }
 }
