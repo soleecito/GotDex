1d49f2875eecb9198d02840bd5bfa87891a44fa2
soleecito <soledadrotiroti@gmail.com>
2021-09-12 20:20:36 -0300

    modificacion de interface api

--- app/src/main/java/com/elvisoperator/gotdex/APIService.kt
+++ app/src/main/java/com/elvisoperator/gotdex/APIService.kt
@@ -1,11 +1,14 @@
 package com.elvisoperator.gotdex
 
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Url
 
 interface APIService {
 
-    @GET
-    suspend fun getCharacterByName(@Url url:String):Response<Book>
+    @GET("characters")
+    fun getCharacters(): Call<List<GotCharacter>>
+
+
 }
--- app/src/main/java/com/elvisoperator/gotdex/CharacterAdapter.kt
+++ app/src/main/java/com/elvisoperator/gotdex/CharacterAdapter.kt
@@ -1,30 +1,5 @@
 package com.elvisoperator.gotdex
-/*
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 
-class CharacterAdapter(val slug:List<String> , val images:List<String>) :
-    RecyclerView.Adapter<CharacterViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        return CharacterViewHolder(layoutInflater.inflate(R.layout.item_character , parent , false))
-    }
-
-    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
-
-        val itemImages = images[position]
-        val itemSlug = slug[position]
-        holder.bind(itemSlug , itemImages)
-
-    }
-
-    override fun getItemCount(): Int {
-        return images.size
-    }
-
-
-}*/
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
