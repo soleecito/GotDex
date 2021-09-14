aefda4672394c7e2c5652234317d471ce4715b59
vao-source-code <vorue@alumno.unlam.edu.ar>
2021-09-14 18:38:45 -0300

    Navigation_Start
    Se implementa la navegacion lateral.

--- app/src/main/java/com/elvisoperator/gotdex/MainActivity.kt
+++ app/src/main/java/com/elvisoperator/gotdex/MainActivity.kt
@@ -3,6 +3,10 @@ package com.elvisoperator.gotdex
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 //import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.elvisoperator.gotdex.databinding.ActivityMainBinding
@@ -16,14 +20,14 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     lateinit var service: APIService
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.recyclerViewCharacter.layoutManager = LinearLayoutManager(this)
 
         val retrofit: Retrofit = Retrofit.Builder()
@@ -35,8 +39,39 @@ class MainActivity : AppCompatActivity() {
 
         obtenerDatos()
 
+        //navigation Start (optimizar a funciones)
+        toggle = ActionBarDrawerToggle(this,binding.drawerLayout,R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+
+
+            when(it.itemId){
+
+                R.id.nav_home -> Toast.makeText(applicationContext, "Click Home" , Toast.LENGTH_LONG).show()
+                R.id.nav_enemy -> Toast.makeText(applicationContext, "Click Enemy" , Toast.LENGTH_LONG).show()
+                R.id.nav_favorite -> Toast.makeText(applicationContext, "Click Favorite" , Toast.LENGTH_LONG).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext, "Click Settings" , Toast.LENGTH_LONG).show()
+                R.id.nav_login -> Toast.makeText(applicationContext, "Click Login" , Toast.LENGTH_LONG).show()
+
+            }
+
+            true
+        }
+        //
+
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
     private fun obtenerDatos() {
         service.getCharacters().enqueue(object : Callback<List<GotCharacter>> {
             override fun onResponse(
